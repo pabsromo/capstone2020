be6cf4ed83d1dbdfa2f0878e7d72f9c94eb6e1b5
addison-maupin <45209908+addisonmaupin@users.noreply.github.com>
2020-04-03 18:16:53 -0500

    display login and signup page

--- tanuki/tanuki/settings.py
+++ tanuki/tanuki/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'tanuki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'tanuki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tanuki',
+        'USER': 'postgres',
+        'PASSWORD': 'codersdontwearsafetyglasses',
+        'HOST': 'localhost',
     }
 }
 
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
