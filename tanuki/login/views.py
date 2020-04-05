a0b7391d842f85049bae099c5ec21f44e052153b
addison-maupin <45209908+addisonmaupin@users.noreply.github.com>
2020-04-05 11:34:59 -0500

    signup page functional

--- tanuki/login/views.py
+++ tanuki/login/views.py
@@ -1,10 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password1']
+
+        user = auth.authenticate
+
+    else:
+        return render(request, 'index.html')
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        firstName = request.POST['first_name']
+        lastName = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username not available.')
+                return redirect('login:signup')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already registered.')
+                return redirect('login:signup')
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email, first_name=firstName, last_name=lastName)
+                user.save()
+
+        else:
+            messages.info(request, 'Passwords do not match.')
+            return redirect('login:signup')
+
+        return redirect('/')
+    else:
+        return render(request, 'signup.html')
