862596625b92e87eba3f89331d866a28f69bffa3
addison-maupin <45209908+addisonmaupin@users.noreply.github.com>
2020-09-17 08:03:19 -0500

    dynamic changes

--- tanuki/budget/views.py
+++ tanuki/budget/views.py
@@ -6,22 +6,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login:index')
 def budget(request):
-    # if request.method == 'POST':
-    #     form = AddItemForm(request.POST, label_suffix=' ')
-    #     if form.is_valid():
-    #         addItem = form.save(commit=False)
-    #         addItem.itemType = form.cleaned_data['itemType']
-    #         addItem.user = request.user
-    #         addItem.save()  # save form after the user and itemType have been determined
-    #         itemName = form.cleaned_data['itemName']
-    #         itemPrice = form.cleaned_data['itemPrice']
-    #         return redirect('overview:home')
-    #     else:
-    #         context = {'form': form}
-    # else:
-    #     # only show objects for authenticated user
-    #     items = AddItem.objects.filter(user=request.user)
-    #     form = AddItemForm(label_suffix=' ')
-
-    #     context = {'form': form, 'items': items}
     return render(request, 'budget.html')
