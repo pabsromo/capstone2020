8f84288dafadd3ab2da0ca679ded53725c4c9bf4
addison-maupin <45209908+addisonmaupin@users.noreply.github.com>
2020-10-06 22:17:15 -0500

    updated budget forms

--- tanuki/budget/forms.py
+++ tanuki/budget/forms.py
@@ -1,25 +1,57 @@
 from django import forms
 
 
-class AddItemForm(forms.ModelForm):
+class SummaryForm(forms.ModelForm)
+    monthlySavings = forms.DecimalField(
+        label = "Monthly Savings",
+        max_digits = 7,
+        decimal_places= 2,
+        widget=forms.TextInput(attrs={'placeholder': 'New Amount'})
+    )
+
+class IncomeForm(forms.ModelForm)
     itemName = forms.CharField(
         label='Item Name',
         max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter Item Name'})
+        widget=forms.TextInput(attrs={'placeholder': 'New Fixed Expense'})
     )
 
-    itemPrice = forms.DecimalField(
-        label='Item Price',
+    itemAmount = forms.DecimalField(
+        label='Item Amount',
         max_digits=7,
         decimal_places=2,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter Item Price'})
+        widget=forms.TextInput(attrs={'placeholder': 'New Amount'})
     )
 
-    itemType = forms.CharField(
-        label='Item Type',
-        max_length=50
+    itemDate = forms.DateField()
+
+
+class FixedExpensesForm(forms.ModelForm):
+    itemName = forms.CharField(
+        label='Item Name',
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'New Fixed Expense'})
     )
 
-    class Meta:
-        model = AddItem
-        fields = ('itemName', 'itemPrice', 'itemType')
+    itemAmount = forms.DecimalField(
+        label='Item Amount',
+        max_digits=7,
+        decimal_places=2,
+        widget=forms.TextInput(attrs={'placeholder': 'New Amount'})
+    )
+
+    itemDate = forms.DateField() 
+
+class InvestingForm(forms.ModelForm):
+    itemName = forms.CharField(
+        label='Item Name',
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'New Account'})
+    )
+
+    itemAmount = forms.DecimalField(
+        label='Item Amount',
+        max_digits=7,
+        decimal_places=2,
+        widget=forms.TextInput(attrs={'placeholder': 'New Amount'})
+    )
