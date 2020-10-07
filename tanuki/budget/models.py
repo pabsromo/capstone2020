7e56b3b461ce25a16431a81e4a99098a874fa6ce
Pablo Romo <33328647+pabsromo@users.noreply.github.com>
2020-10-06 22:00:00 -0500

    Updating Models for Budget

--- tanuki/budget/models.py
+++ tanuki/budget/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 # Create your models here.
-
 class Summary(models.Model):
+    AvailableCash = models.DecimalField(max_digits=7, decimal_places=2)
 
 class Income(models.Model):
+    itemDate = models.DateTimeField(auto_now_add=False, blank=True) # equivalent to dateDisplayed from AddItem
+    itemName = models.CharField(max_length = 100)
+    itemAmount = models.DecimalField(max_digits=7, decimal_places=2) # equivalent to itemPrice from AddItem
 
 class FixedExpenses(models.Model):
+    itemDate = models.DateTimeField(auto_now_add=False, blank=True) # The due date for the expense
+    itemName = models.CharField(max_length = 100)
+    itemAmount = models.DecimalField(max_digits=7, decimal_places=2)
 
 class Savings(models.Model):
+    itemName = models.CharField(max_length = 100)
+    itemAmount = models.CharField(max_length = 100)
 
 class Investing(models.Model):
+    itemName = models.CharField(max_length = 100)
+    itemAmount = models.DecimalField(max_digits=7, decimal_places=2)
