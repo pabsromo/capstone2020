76653280fa9caf6dcc6614926547ec08526414c2
Pablo Romo <33328647+pabsromo@users.noreply.github.com>
2020-10-06 20:28:14 -0500

    Working so far
    Working so far with the Overview page, Budget Page, and History Page.

--- tanuki/budget/views.py
+++ tanuki/budget/views.py
@@ -3,7 +3,23 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login:index')   #redirect to login if user has not been authenticated
+def budget(request): 
+    if request.method == 'POST':
+        form = AddItemForm(request.POST, label_suffix =' ')
+        if form.is_valid():
+            addItem = form.save(commit=False)
+            addItem.itemType = form.cleaned_data['itemType']
+            addItem.user = request.user
+            addItem.save()    #save form after the user and itemType have been determined
+            itemName = form.cleaned_data['itemName']
+            itemPrice = form.cleaned_data['itemPrice']
+            return redirect('overview:home')
+        else:
+            context = {'form': form}  
+    else:
+        items = AddItem.objects.filter(user=request.user)    #only show objects for authenticated user
+        form = AddItemForm(label_suffix=' ')
 
-@login_required(login_url='login:index')
-def budget(request):
-    return render(request, 'budget.html')
+        context = {'form': form, 'items': items}
+    return render(request, 'budget.html', context)
