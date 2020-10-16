6ce508f519e439f34ef044c47f1f714966c1b672
addison-maupin <45209908+addisonmaupin@users.noreply.github.com>
2020-10-15 20:25:35 -0500

    daterange for history page updated

--- tanuki/history/templatetags/custom_history_tags.py
+++ tanuki/history/templatetags/custom_history_tags.py
@@ -1,4 +1,5 @@
 from django import template
+import datetime
 
 register = template.Library()
 
@@ -6,3 +7,19 @@ register = template.Library()
 def modulo(num, val):
     return num % val
 
+
+@register.simple_tag
+def findWeek():
+    today = datetime.date.today()
+    monday = today - datetime.timedelta(days=today.weekday())
+    daysTillSunday = (6-today.weekday()) % 7
+    sunday = today + datetime.timedelta(days=daysTillSunday)
+
+    month1 = monday.strftime("%B")
+    month2 = sunday.strftime("%B")
+    day1 = monday.strftime("%#d")
+    day2 = sunday.strftime("%#d")
+
+    string = month1 + " " + day1 + " - " + month2 + " " + day2
+    return string
+    
