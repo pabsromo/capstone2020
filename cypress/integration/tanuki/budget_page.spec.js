27c347838ff075e0961c05cee3e9f1284752defa
addison-maupin <45209908+addisonmaupin@users.noreply.github.com>
2020-11-04 13:18:56 -0600

    budget page validation

--- cypress/integration/tanuki/budget_page.spec.js
+++ cypress/integration/tanuki/budget_page.spec.js
@@ -1,36 +1,51 @@
-/// <reference types="cypress" />
-
-Cypress.Cookies.defaults({
-    preserve: "csrftoken"
-})
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-  })
 
 context('Home Page', () => {
+
+  before('Login', () => {
+    // log in only once before any of the tests run.
+    cy.visit('http://127.0.0.1:8000/')
+
+    cy.get('#username')
+      .type('test', { delay: 100 })
+      .should('have.value', 'test')
+
+    cy.get('#password')
+      .type('tanuki404', { delay: 100 })
+      .should('have.value', 'tanuki404')
+
+    cy.get('.userinfo')
+      .find('form').submit()
+
+    cy.url().should('eq', 'http://127.0.0.1:8000/home/')
+    cy.get('#budget1').click()
+    cy.url().should('eq', 'http://127.0.0.1:8000/budget/')
+  })
+
+  beforeEach(() => {
+    // before each test, we can automatically preserve the
+    // 'session_id' and 'remember_token' cookies. 
+    Cypress.Cookies.preserveOnce('csrftoken', 'sessionid')
+  })
     
-    // Logging in
-    it('Logging in', () => {
-        cy.visit('http://127.0.0.1:8000/')
+    // // Logging in
+    // it('Logging in', () => {
+    //     cy.visit('http://127.0.0.1:8000/')
 
-        cy.get('#username')
-          .type('test')
-          .should('have.value', 'test')
+    //     cy.get('#username')
+    //       .type('test')
+    //       .should('have.value', 'test')
 
-        cy.get('#password')
-          .type('tanuki404')
-          .should('have.value', 'tanuki404')
+    //     cy.get('#password')
+    //       .type('tanuki404')
+    //       .should('have.value', 'tanuki404')
 
-        cy.get('.userinfo')
-          .find('form').submit()
+    //     cy.get('.userinfo')
+    //       .find('form').submit()
 
-        cy.get('#budget1').click()
+    //     cy.get('#budget1').click()
 
-        cy.url().should('eq', 'http://127.0.0.1:8000/budget/')
-    })
+    //     cy.url().should('eq', 'http://127.0.0.1:8000/budget/')
+    // })
 
     // users can add items to income
     it('users can add items to income', () => {
--- tanuki/budget/templatetags/custom_budget_tags.py
+++ tanuki/budget/templatetags/custom_budget_tags.py
@@ -1,6 +1,14 @@
 from django import template
+import datetime
 
 register = template.Library()
 @register.filter
 def modulo(num, val):
     return num % val
+
+
+@register.simple_tag
+def findMonth():
+    today = datetime.date.today()
+    month = today.strftime("%B")
+    return month
