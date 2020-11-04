807245844070367d8fc6eee0c76ef1661f56d46b
addison-maupin <45209908+addisonmaupin@users.noreply.github.com>
2020-11-03 22:42:46 -0600

    login validation tests

--- cypress/integration/tanuki/login_page.spec.js
+++ cypress/integration/tanuki/login_page.spec.js
@@ -10,8 +10,47 @@ context('Login Page', () => {
     })
 
     // Test to make sure the user can get to signup and get back to login
+    it('Navigate to Signup Page and back to Login Page', () =>{
+      cy.get('.signup').click()
+      cy.url().should('eq', 'http://127.0.0.1:8000/signup/')
+
+    })
 
     // Test to make sure you can't access the webpages without logging in
+
+    // Test invalid username
+    it('login with incorrect username', () => {
+      cy.get('#username')
+        .type('abcde', { delay: 100 })
+        .should('have.value', 'abcde')
+
+      cy.get('#password')
+        .type('tanuki404', { delay: 100 })
+        .should('have.value', 'tanuki404')
+
+      cy.get('.userinfo')
+        .find('form').submit()
+
+      cy.get('#errormessage')
+        .should('contain', 'Invalid credentials, please try again.' )
+    })
+
+    // Test invalid password
+    it('login with incorrect password', () => {
+      cy.get('#username')
+        .type('test', { delay: 100 })
+        .should('have.value', 'test')
+
+      cy.get('#password')
+        .type('tanuki404*', { delay: 100 })
+        .should('have.value', 'tanuki404*')
+
+      cy.get('.userinfo')
+        .find('form').submit()
+
+      cy.get('.account')
+        .should('contain', 'Invalid credentials, please try again.')
+    })
 
     // Able to login the user
     it('login with test user', () => {
