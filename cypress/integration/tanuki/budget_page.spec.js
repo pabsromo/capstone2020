0a2da3361a0085f4a83bfa021981198dd3904634
Pablo Romo <33328647+pabsromo@users.noreply.github.com>
2020-11-14 10:46:56 -0600

    Updated Budget Page validation tests

--- cypress/integration/tanuki/budget_page.spec.js
+++ cypress/integration/tanuki/budget_page.spec.js
@@ -26,69 +26,66 @@ context('Home Page', () => {
     // 'session_id' and 'remember_token' cookies. 
     Cypress.Cookies.preserveOnce('csrftoken', 'sessionid')
   })
+
+  it('testing datepicker', () => {
+    cy.get('#id_itemDate')
+      .type('2020-11-05')
+  })
+
+  // Users can add and delete items to income
+  it('Users can add and delete income items', () => {
     
-    // // Logging in
-    // it('Logging in', () => {
-    //     cy.visit('http://127.0.0.1:8000/')
+    // Add new income item
+    cy.get('#id_itemDate')
+      .type('2020-11-05')
+      .should('have.value', '2020-11-05')
+      
+    cy.get('#id_itemName')
+      .type('Piggybank')
+      .should('have.value', 'Piggybank')
 
-    //     cy.get('#username')
-    //       .type('test')
-    //       .should('have.value', 'test')
+    cy.get('#id_itemAmount')
+      .type('200')
+      .should('have.value', '200')
 
-    //     cy.get('#password')
-    //       .type('tanuki404')
-    //       .should('have.value', 'tanuki404')
+    cy.get('#id_itemAmount')
+      .type('{enter}')
 
-    //     cy.get('.userinfo')
-    //       .find('form').submit()
+    // Check that item was entered correctly
+    
+    // Delete the item
+    cy.get("#deleteincome").click()
 
-    //     cy.get('#budget1').click()
-
-    //     cy.url().should('eq', 'http://127.0.0.1:8000/budget/')
-    // })
-
-    // users can add and delete items to income
-    it('Users can add and delete income items', () => {
-        cy.get('#id_itemDate')
-          .type('11/5/2020')
-          .should('have.value', '11/5/2020')
-        
-        cy.get('#id_itemName')
-          .type('Piggybank')
-          .should('have.value', 'Piggybank')
-
-        cy.get('#id_itemAmount')
-          .type('200')
-          .should('have.value', '200')
-
-        cy.get('#id_itemAmount')
-          .type('{enter}')
-        
-        cy.get("#deleteincome").click()
-    })
+    // Check that item was deleted correctly
+  })
 
 
-    // users can add and delete items to fixed expenses
-    it('Users can add and delete fixed expenses items', () => {
-        cy.get('.fixed-expenses-table-column').get("#id_itemDate")
-          .type('11/5/2020')
-          .should('have.value', '11/5/2020')
+  // users can add and delete items to fixed expenses
+  it('Users can add and delete fixed expenses items', () => {
+    
+    // Add new fixed expenses item
+    cy.get('.fixed-expenses-table-column').get("#id_itemDate")
+      .type('2020-11-05')
+      .should('have.value', '2020-11-05')
 
-        cy.get('#id_itemName')
-          .type('Piggybank')
-          .should('have.value', 'Piggybank')
+    cy.get('#id_itemName')
+      .type('Piggybank')
+      .should('have.value', 'Piggybank')
 
-        cy.get('#id_itemAmount')
-          .type('200')
-          .should('have.value', '200')
+    cy.get('#id_itemAmount')
+      .type('200')
+      .should('have.value', '200')
 
-        cy.get('#id_itemAmount')
-          .type('{enter}')
+    cy.get('#id_itemAmount')
+      .type('{enter}')
 
-        cy.get("#deletefixed").click()
-    })
+    // Check that item was entered correctly
 
-  
+    // Delete the item
+    cy.get("#deletefixed").click()
+
+    // Check that the item was deleted correctly
+  })
 
     // users can adjust where their additional spending goes
 
@@ -100,6 +97,27 @@ context('Home Page', () => {
         
     })
 
-    // users should be able to navigate to other pages and log out
+    // // Navigate between pages
+    // it('Navigate to other pages (Home, Budget, History) as authenticated user', () => {
+    //   cy.get('#home1').click()
+    //   cy.url().should('eq', 'http://127.0.0.1:8000/home/')
+      
+    //   cy.get('#budget1').click()
+    //   cy.url().should('equal', 'http://127.0.0.1:8000/budget/')
 
+    //   cy.get('#home1').click()
+    //   cy.url().should('eq', 'http://127.0.0.1:8000/home/')
+
+    //   cy.get('#history1').click()
+    //   cy.url().should('eq', 'http://127.0.0.1:8000/history/')
+
+    //   cy.get('#home1').click()
+    //   cy.url().should('eq', 'http://127.0.0.1:8000/home/')
+    // })
+
+    // // Logout user
+    // it('Logout', () => {
+    //     cy.visit('http://127.0.0.1:8000/logout/')    
+    //     cy.url().should('eq', 'http://127.0.0.1:8000/')
+    // })
 })
